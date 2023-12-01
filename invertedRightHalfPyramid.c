74cd1510721fdad0ba9b2d6d72c9b79ae5035079
Nikhil M B <nikhilaz787@gmail.com>
2023-12-01 23:21:52 +0530

    Program to print inverted rignt half pyramid

---
+++ invertedRightHalfPyramid.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main(){
+
+	int i, j, rows;
+
+	printf("Enter the no. of rows: ");
+	scanf("%d", &rows);
+
+	for(i = 0; i < rows; i++){
+	
+		for(j = 0; j < rows - i; j++)
+
+			printf("* ");
+	
+		printf("\n");
+	
+	
+	}
+
+
+
+
+
+
+}
