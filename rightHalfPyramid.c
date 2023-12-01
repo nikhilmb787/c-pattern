2c0d54817e585bca2dd99c58423e123784ad1970
Nikhil M B <nikhilaz787@gmail.com>
2023-12-01 22:27:23 +0530

    program to print the rightHalfPyramid

---
+++ rightHalfPyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+
+	int i, j, rows;
+
+	printf("Enter the no. of rows: ");
+	scanf("%d", &rows);
+
+	for(i = 0; i < rows; i++){
+	
+		for(j = 0; j <= i; j++)
+
+			printf("* ");
+
+		printf("\n");
+	}
+	
+}
